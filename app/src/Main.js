76b44a8bf4c9eaf134ad38ac49f9b8b64cbabca4
RyotArch <bboy.ryu@icloud.com>
2018-09-24 22:47:02 -0700

    Create main page
    Add Main.js and edit base index.html

---
+++ app/src/Main.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+  render() {
+    return (
+        <div>
+          <h1>Single Page Application</h1>
+          <ul className="header">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+          <div className="content">
+          </div>
+        </div>
+    );
+  }
+}
+
+export default Main;

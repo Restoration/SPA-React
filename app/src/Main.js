d7796e1cbd571e8abfeb39e50679033f1e354d02
RyotArch <bboy.ryu@icloud.com>
2018-09-24 22:53:15 -0700

    Routing process
    Edit routing

--- app/src/Main.js
+++ app/src/Main.js
@@ -1,20 +1,32 @@
 import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  BrowserRouter
+} from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
 
 class Main extends Component {
   render() {
     return (
+      <BrowserRouter>
         <div>
           <h1>Single Page Application</h1>
           <ul className="header">
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/about">About</NavLink></li>
+            <li><NavLink to="/contact">Contact</NavLink></li>
           </ul>
           <div className="content">
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/contact" component={Contact}/>
           </div>
         </div>
+      </BrowserRouter>
     );
   }
 }
-
 export default Main;

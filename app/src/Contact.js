a209de497596707143c79c86653d17f0c82a4fc6
RyotArch <bboy.ryu@icloud.com>
2018-09-24 22:49:29 -0700

    Create content page
    Add Home.js,About.js and Contact.js

---
+++ app/src/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Contact</h2>
+        <p>Please feel free to contact me</p>
+        <p>info@example.com</p>
+      </div>
+    );
+  }
+}
+
+export default Contact;
---
+++ app/src/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>HELLO</h2>
+        <p>Hello World!! This page is top page. </div>
+    );
+  }
+}
+
+export default Home;
